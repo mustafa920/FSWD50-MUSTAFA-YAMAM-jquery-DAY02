521b04f34820cdd17c03d85e1a3ad4e4926df3cf
mustafa920 <tafocosmos@gmail.com>
2018-10-02 10:45:45 +0200

    ex-01-02

---
+++ ex-1-2/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#demo2').on('click', function() {
+        $("p").text("You clicked on the Christmas bell");
+    })
+ 
+$('.santa').click( function() {
+ $(this).hide()
+})
+
+});
